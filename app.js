959adf0979f0c612e59c06c62b8e4157b8c7a68d
Nasereddinereda <nasereddinebekkal@gmail.com>
2019-10-17 01:11:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// global variable used in window.onscroll function
+var addwrt = false;
+
+window.onscroll = function() {
+  const navbar = document.getElementById("navbar");
+  const navbar1 = document.getElementById("navbar1");
+  const writing = document.getElementById("writing");
+  let child;
+  if (
+    document.documentElement.scrollTop > 50 &&
+    document.documentElement.scrollTop < 200
+  ) {
+    navbar.className = "BG-col TR-dur wh-col ";
+    navbar1.className = "d-none";
+    if (addwrt === false) {
+      child = document.createElement("span");
+      child.innerHTML = " : NEW YORK";
+      child.className = "p-5 ft-5 ";
+      writing.appendChild(child);
+      addwrt = true;
+    }
+  } else if (document.documentElement.scrollTop > 200) {
+    navbar.className = "BG-col TR-dur wh-col ";
+    navbar1.className = "d-flex BG-col wh-col ";
+    if (addwrt === false) {
+      child = document.createElement("span");
+      child.innerHTML = " : NEW YORK";
+      child.className = "p-5 ft-5 ";
+      writing.appendChild(child);
+      addwrt = true;
+    }
+  } else if (document.documentElement.scrollTop < 50) {
+    navbar.className = "BG-ncol TR-dur wh-col ";
+    writing.innerHTML = "DATA USA";
+    addwrt = false;
+  }
+};
